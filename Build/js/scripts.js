d73be476fb03230b9041e5c8b5c66622be7112c5
Steve Schofield <stevebschofield@gmail.com>
2015-07-24 16:16:25 +0100

    added weather gimmick

--- Build/js/scripts.js
+++ Build/js/scripts.js
@@ -321,5 +321,56 @@ $(window).load(function() {
         }, 500);
     }, 10);
 
+    // Weather Interaction
+
+    $('#logo-icon').hover(function(){
+        $('.js-geolocation').toggleClass('hide');
+    });
 
 }); 
+
+$(document).ready(function() {
+  loadWeather('Bournemouth',''); //@params location, woeid
+});
+
+// Weather
+
+    /* Does your browser support geolocation? */
+if ("geolocation" in navigator) {
+  $('.js-geolocation').show(); 
+} else {
+  $('.js-geolocation').hide();
+}
+
+/* Where in the world are you? */
+$('.js-geolocation').on('click', function() {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    loadWeather(position.coords.latitude+','+position.coords.longitude); //load weather using your lat/lng coordinates
+  });
+});
+
+/* 
+* Test Locations
+* Austin lat/long: 30.2676,-97.74298
+* Austin WOEID: 2357536
+*/
+
+function loadWeather(location, woeid) {
+  $.simpleWeather({
+    location: location,
+    woeid: woeid,
+    unit: 'f',
+    success: function(weather) {
+      html = '<h2><i class="weather icon-'+weather.code+'"></i></h2>';
+      // '+weather.temp+'&deg;'+weather.units.temp+'
+      html += '<span class="weather-location rotate">'+weather.city+' <br/> '+weather.region+'</span>';
+      // html += '<li class="currently">'+weather.currently+'</li>';
+      // html += '<li>'+weather.alt.temp+'&deg;C</li></ul>';  
+      
+      $("#weather").html(html);
+    },
+    error: function(error) {
+      $("#weather").html('<p>'+error+'</p>');
+    }
+  });
+}
